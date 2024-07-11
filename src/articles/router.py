47546e81d245f309bd7bb2f633c0922015742879
iuzimin <iuzimin@croc.ru>
2024-07-11 17:21:36 +0300

    added get book and get article endpoints

--- src/articles/router.py
+++ src/articles/router.py
@@ -16,3 +16,11 @@ async def get_all_books(session: AsyncSession = Depends(get_async_session)):
     result = await session.execute(query)
     articles = result.scalars().all()
     return articles
+
+
+@router.get(path="/article_{article_id}")
+async def get_book(article_id: int, session: AsyncSession = Depends(get_async_session)):
+    query = select(Articles).where(Articles.id == article_id)
+    result = await session.execute(query)
+    article = result.scalars().all()
+    return article
--- src/books/router.py
+++ src/books/router.py
@@ -8,7 +8,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_async_session
 from src.books.models import Books
 
-
 router = APIRouter(prefix="/books", tags=["Books"])
 
 templates = Jinja2Templates(directory="templates")
@@ -20,3 +19,11 @@ async def get_all_books(request: Request, session: AsyncSession = Depends(get_as
     result = await session.execute(query)
     books = result.scalars().all()
     return templates.TemplateResponse("books.html", {"request": request, "books": books})
+
+
+@router.get(path="/book_{book_id}")
+async def get_book(book_id: int, session: AsyncSession = Depends(get_async_session)):
+    query = select(Books).where(Books.id == book_id)
+    result = await session.execute(query)
+    book = result.scalars().all()
+    return book
