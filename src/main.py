fdb7156886167ab6489db3839bb7108ce774a0ac
iuzimin <iuzimin@croc.ru>
2024-07-19 12:40:52 +0300

    optimized main.py

--- src/main.py
+++ src/main.py
@@ -1,37 +1,18 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
-from starlette.responses import HTMLResponse
 
-from src.queries import reset_database
 from src.books.router import router as router_books
 from src.articles.router import router as router_articles
-from src.router import router as router_index
-
-from contextlib import asynccontextmanager
+from src.pages.router import router as router_pages
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await reset_database()
-    yield
-    print("Выключение")
-
-
-app = FastAPI(
-    title="Blog of Yassya Lil"
-    # lifespan=lifespan
-)
+app = FastAPI(title="Blog of Yassya Lil")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-app.include_router(router_index)
+app.include_router(router_pages)
 app.include_router(router_books)
 app.include_router(router_articles)
-
-
-
-
 
 
 # if __name__ == "__main__":
