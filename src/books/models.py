9f3ff88205224657fee88a1f733d03db8e30f751
julianzim <ziminjulian@gmail.com>
2024-09-21 17:02:13 +0300

    added reviews model

--- src/books/models.py
+++ src/books/models.py
@@ -1,7 +1,9 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from src.database import Base
-
 import datetime
+from typing import Annotated
+from sqlalchemy import text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
 
 
 class Books(Base):
@@ -12,10 +14,22 @@ class Books(Base):
     series: Mapped[str]
     short_description: Mapped[str]
     full_description: Mapped[str]
-    pub_date: Mapped[datetime.datetime]
+    pub_date: Annotated[datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
     author: Mapped[str]
     image: Mapped[str]
     pages: Mapped[int]
     language: Mapped[str]
     age: Mapped[str]
     reviews_count: Mapped[int]
+
+
+class Reviews(Base):
+    __tablename__ = "reviews"
+
+    id: Mapped[int] - mapped_column(primary_key=True)
+    book: mapped_column(ForeignKey("books.id", ondelete="CASCADE"))
+    reviewer: mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    rating: Mapped[int]
+    title: Mapped[str]
+    description: Mapped[str]
+    created_at: Annotated[datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
