537cc0bd144e18a89246c0355ac551f76520377a
julianzim <ziminjulian@gmail.com>
2024-10-06 16:32:27 +0300

    added redirection to /auth/login on after success register

--- static/scripts/registration.js
+++ static/scripts/registration.js
@@ -14,9 +14,9 @@ document.getElementById('registration-form').addEventListener('submit', async fu
     document.getElementById('password-strength').textContent = '';
 
     if (data.password !== data.repeat_password) {
-            document.getElementById('repeat-password-error').textContent = 'Passwords do not match';
-            return;
-        }
+        document.getElementById('repeat-password-error').textContent = 'Passwords do not match';
+        return;
+    }
 
     const response = await fetch('/auth/register', {
         method: 'POST',
@@ -26,9 +26,18 @@ document.getElementById('registration-form').addEventListener('submit', async fu
         body: JSON.stringify(data)
     });
 
-    const result = await response.json();
-    console.log('Form submitted with:', { username, email, password });
-    console.log(result);
+    if (response.ok) {
+        // Если ответ успешный, перенаправляем на страницу входа
+        window.location.href = '/auth/login';
+    } else {
+        // Обработка ошибок, если регистрация не удалась
+        const result = await response.json();
+        console.error('Registration failed:', result);
+        // Вы можете отобразить ошибку пользователю, если это необходимо
+        if (result.message) {
+            document.getElementById('email-error').textContent = result.message;
+        }
+    }
 });
 
 document.getElementById('password').addEventListener('input', function() {
