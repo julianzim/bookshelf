f0354e5addb502af24c60c609c071684c33e0124
julianzim <ziminjulian@gmail.com>
2024-10-06 16:31:21 +0300

    added review create handler

--- src/books/router.py
+++ src/books/router.py
@@ -1,12 +1,15 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form
+from fastapi.responses import RedirectResponse, Response
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from typing import List
+from typing import List, Optional
 
+
+from src.auth.base_config import current_user
 from src.database import get_async_session
-from src.books.models import Books
+from src.books.models import Books, Reviews
 from src.books.schemas import GetAllBooks
 
 router = APIRouter(prefix="/books", tags=["Books"])
@@ -35,3 +38,31 @@ async def get_related_books(book_name: str, session: AsyncSession = Depends(get_
     result = await session.execute(query)
     books = result.scalars().all()
     return books
+
+
+@router.post(path="/{book_name}/review")
+async def create_review(
+    book_name: str,
+    title: str = Form(...),
+    description: str = Form(...),
+    rating: int = Form(...),
+    session: AsyncSession = Depends(get_async_session),
+    curr_user=Depends(current_user),
+):
+
+    query = select(Books).where(Books.title == book_name)
+    result = await session.execute(query)
+    book = result.scalars().one()
+
+    new_review = Reviews(
+        book=book.id,
+        reviewer=curr_user.id,
+        title=title,
+        description=description,
+        rating=rating
+    )
+    session.add(new_review)
+    await session.commit()
+    await session.refresh(new_review)
+
+    return {"message": "Review created successfully", "review_id": new_review.id}
