f20667e8d7d81ebd7f226b79df9dbda0b2a23492
julianzim <ziminjulian@gmail.com>
2024-10-13 13:11:58 +0300

    added current_user parameter for all endpoints

--- src/pages/router.py
+++ src/pages/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, Request
 from starlette.templating import Jinja2Templates
 
+from src.auth.base_config import current_user_optional
 from src.books.router import get_all_books, get_book, get_related_books
 from src.articles.router import get_all_articles
 
@@ -13,35 +14,101 @@ templates = Jinja2Templates(directory="templates/")
 
 
 @router.get("/")
-async def get_home(request: Request, books=Depends(get_all_books)):
-    return templates.TemplateResponse("pages/home.html", {"request": request, "books": books})
+async def get_home(
+        request: Request,
+        books=Depends(get_all_books),
+        current_user=Depends(current_user_optional)
+):
+    return templates.TemplateResponse(
+        "pages/home.html", {
+            "request": request,
+            "books": books,
+            "current_user": current_user
+        }
+    )
 
 
 @router.get("/about")
-async def get_about(request: Request):
-    return templates.TemplateResponse("pages/about.html", {"request": request})
+async def get_about(
+        request: Request,
+        current_user=Depends(current_user_optional)
+):
+    return templates.TemplateResponse(
+        "pages/about.html", {
+            "request": request,
+            "current_user": current_user
+        }
+    )
 
 
 @router.get("/books")
-async def get_books(request: Request, books=Depends(get_all_books)):
-    return templates.TemplateResponse("pages/books.html", {"request": request, "books": books})
+async def get_books(
+        request: Request,
+        books=Depends(get_all_books),
+        current_user=Depends(current_user_optional)
+):
+    return templates.TemplateResponse(
+        "pages/books.html", {
+            "request": request,
+            "books": books,
+            "current_user": current_user
+        }
+    )
 
 
 @router.get("/books/{book_name}")
-async def get_book_detail(request: Request, book=Depends(get_book), books=Depends(get_related_books)):
-    return templates.TemplateResponse("pages/book_details.html", {"request": request, "book": book, "books": books})
+async def get_book_detail(
+        request: Request,
+        book=Depends(get_book),
+        books=Depends(get_related_books),
+        current_user=Depends(current_user_optional)
+):
+    return templates.TemplateResponse(
+        "pages/book_details.html", {
+            "request": request,
+            "book": book,
+            "books": books,
+            "current_user": current_user
+        }
+    )
 
 
 @router.get("/blog")
-async def get_blog(request: Request, articles=Depends(get_all_articles)):
-    return templates.TemplateResponse("pages/blog.html", {"request": request, "articles": articles})
+async def get_blog(
+        request: Request,
+        articles=Depends(get_all_articles),
+        current_user=Depends(current_user_optional)
+):
+    return templates.TemplateResponse(
+        "pages/blog.html", {
+            "request": request,
+            "articles": articles,
+            "current_user": current_user
+        }
+    )
 
 
 @router.get("/auth/register")
-async def get_registration(request: Request):
-    return templates.TemplateResponse("auth_pages/registration.html", {"request": request})
+async def get_registration(
+        request: Request,
+        current_user=Depends(current_user_optional)
+):
+    return templates.TemplateResponse(
+        "auth_pages/registration.html", {
+            "request": request,
+            "current_user": current_user
+        }
+    )
 
 
 @router.get("/auth/login")
-async def get_registration(request: Request):
-    return templates.TemplateResponse("auth_pages/login.html", {"request": request})
+async def get_registration(
+        request: Request,
+        current_user=Depends(current_user_optional)
+):
+    return templates.TemplateResponse(
+        "auth_pages/login.html", {
+            "request": request,
+            "current_user": current_user
+        }
+    )
