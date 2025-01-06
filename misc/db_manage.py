e1a73a23a00f166e8b7d0d0fa010e45ac99ad64c
Julian Zimin <julianzimin@gmail.com>
2025-01-06 16:37:16 +0300

    fixed problem with "No module named src"

--- misc/db_manage.py
+++ misc/db_manage.py
@@ -1,4 +1,8 @@
 import asyncio
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.database import async_session
 from src.queries import reset_database
