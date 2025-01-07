a321600cd596cc2e11081d0b97776d7c36f26bfc
Julian Zimin <julianzimin@gmail.com>
2025-01-07 21:34:12 +0300

    deleted handler for book details page endpoint

--- src/pages/router.py
+++ src/pages/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Request
 from starlette.templating import Jinja2Templates
 
 from src.auth.base_config import current_user_optional
-from src.books.router import get_book, get_related_books
+# from src.books.router import get_book, get_related_books
 
 
 router = APIRouter(
@@ -20,23 +20,6 @@ async def get_about(
     return templates.TemplateResponse(
         "pages/about.html", {
             "request": request,
-            "current_user": current_user
-        }
-    )
-
-
-@router.get("/books/{book_name}")
-async def get_book_detail(
-    request: Request,
-    book=Depends(get_book),
-    books=Depends(get_related_books),
-    current_user=Depends(current_user_optional)
-):
-    return templates.TemplateResponse(
-        "pages/book_details.html", {
-            "request": request,
-            "book": book,
-            "books": books,
             "current_user": current_user
         }
     )
