037689f194e54a3afea00eb3acda55a6eb5e5467
Julian Zimin <julianzimin@gmail.com>
2025-01-07 20:17:13 +0300

    deleted handler for home page endpoint

--- src/pages/router.py
+++ src/pages/router.py
@@ -1,13 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from starlette.templating import Jinja2Templates
 
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.database import get_async_session
 from src.auth.base_config import current_user_optional
 from src.books.router import get_book, get_related_books
-from src.books.models import Books
 
 
 router = APIRouter(
@@ -15,26 +10,6 @@ router = APIRouter(
 )
 
 templates = Jinja2Templates(directory="templates/")
-
-
-@router.get("/")
-async def get_home(
-    request: Request,
-    current_user=Depends(current_user_optional),
-    session: AsyncSession = Depends(get_async_session)
-):
-    query = select(Books.id, Books.title, Books.image)
-    result = await session.execute(query)
-    books = result.fetchall()
-    books_rows = [{"id": book[0], "title": book[1], "image": book[2]} for book in books]
-    
-    return templates.TemplateResponse(
-        "pages/home.html", {
-            "request": request,
-            "books": books_rows,
-            "current_user": current_user
-        }
-    )
 
 
 @router.get("/about")
