b53652bfbe4280a92b741d51b33d06b5e5618ac7
Julian Zimin <julianzimin@gmail.com>
2025-01-07 21:10:16 +0300

    added queries for book details, related books and book reviews

--- src/queries.py
+++ src/queries.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.books.models import Books, Reviews
 from src.database import async_engine, Base
 
 
@@ -9,3 +11,34 @@ async def reset_database():
         await conn.run_sync(Base.metadata.create_all)
     # yield
     print('База данных сброшена')
+
+
+async def get_book_by_title(
+    title: str,
+    session: AsyncSession
+):
+    query = select(Books).where(Books.title == title)
+    result = await session.execute(query)
+    book = result.scalars().one()
+    return book
+
+
+async def get_related_books_by_title(
+    title: str,
+    session: AsyncSession
+):
+    query = select(Books)
+    result = await session.execute(query)
+    books = result.scalars().all()
+    return books
+
+
+async def get_all_book_reviews(
+    title: str,
+    session: AsyncSession
+):
+    query = select(Reviews)
+    result = await session.execute(query)
+    reviews = result.scalars().all()
+
+    return reviews
