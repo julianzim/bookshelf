5156d9ac2941a01aa92d1a8da0a8c1e1a4127e21
Julian Zimin <julianzimin@gmail.com>
2025-01-08 12:33:03 +0300

    deleted reviews model

--- src/books/models.py
+++ src/books/models.py
@@ -26,15 +26,3 @@ class Books(Base):
     mean_rating: Mapped[float]
     amazon_link: Mapped[str]
     aloud_link: Mapped[str]
-
-
-class Reviews(Base):
-    __tablename__ = "reviews"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    book: Mapped[int] = mapped_column(ForeignKey("books.id", ondelete="CASCADE"))
-    reviewer: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
-    rating: Mapped[int]
-    title: Mapped[str]
-    text: Mapped[str]
-    created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
