07e6b3205f668fc37031cae7252776d51b43153a
Julian Zimin <julianzimin@gmail.com>
2025-01-08 12:32:34 +0300

    added reviews model import

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,8 @@ from sqlalchemy import pool
 from alembic import context
 
 from src.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS
-from src.books.models import Books, Reviews
+from src.books.models import Books
+from src.reviews.models import Reviews
 from src.articles.models import Articles
 from src.auth.models import Role, User
 from src.database import Base
