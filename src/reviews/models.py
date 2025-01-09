d8c30f0236eac4f5e2894d5ef6e9d10837843c92
Julian Zimin <julianzimin@gmail.com>
2025-01-09 21:08:13 +0300

    changed nullable of columns

--- src/reviews/models.py
+++ src/reviews/models.py
@@ -12,7 +12,10 @@ class Reviews(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     book: Mapped[int] = mapped_column(ForeignKey("books.id", ondelete="CASCADE"))
     reviewer: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
-    rating: Mapped[int]
-    title: Mapped[str]
-    text: Mapped[str]
-    created_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+        nullable=False
+    )
+    rating: Mapped[int] = mapped_column(nullable=False)
+    title: Mapped[str] = mapped_column(nullable=True)
+    text: Mapped[str] = mapped_column(nullable=True)
