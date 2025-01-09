2cff0af6ebd3f7b6b0ef48d7dc3f1dfe0acdd376
Julian Zimin <julianzimin@gmail.com>
2025-01-09 17:17:45 +0300

    updated create review schema

--- src/reviews/schemas.py
+++ src/reviews/schemas.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ReviewCreate(BaseModel):
-    title: str
-    text: str
-    rating: int
+    title: str = Field(min_length=3, max_length=100, description="Title of the review")
+    text: str = Field(min_length=10, max_length=1000, description="Text of the review")
+    rating: int = Field(ge=1, le=5, description="Rating between 1 and 5")
