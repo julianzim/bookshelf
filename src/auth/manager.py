cf21e6df84ce688880f7c78c0a1554470951c214
Julian Zimin <julianzimin@gmail.com>
2025-01-09 16:06:16 +0300

    added logging

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -7,14 +7,17 @@ from fastapi.responses import Response
 from src.auth.models import User
 from src.auth.utils import get_user_db
 from src.config import SECRET_AUTH
+from misc.utils import get_logger
 
+
+logger = get_logger(__name__)
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     reset_password_token_secret = SECRET_AUTH
     verification_token_secret = SECRET_AUTH
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
-        print(f"User {user.id} has registered.")
+        logger.info(f"User {user.id} has registered")
 
     async def on_after_login(
         self,
@@ -22,11 +25,12 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         request: Optional[Request] = None,
         response: Optional[Response] = None,
     ) -> None:
-        print(f"User {user.id} {user.username} has login.")
+        logger.info(f"User {user.id} {user.username} has login")
         if response:
             last_page = request.query_params.get("next") or request.headers.get("Referer") or "/"
             response.status_code = 302
             response.headers["Location"] = last_page
+            logger.debug(f"User {user.id} redirected to the page {last_page}")
 
     async def create(
         self,
@@ -41,6 +45,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
 
         existing_user = await self.user_db.get_by_email(user_create.email)
         if existing_user is not None:
+            logger.warning(f"User with email {user_create.email} already exists")
             raise exceptions.UserAlreadyExists()
 
         user_dict = (
