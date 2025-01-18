40c6701d75cf841545440871b5a4e4ee285e96c4
Julian Zimin <julianzimin@gmail.com>
2025-01-18 16:10:39 +0300

    edited script

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -3,27 +3,31 @@ from pathlib import Path
 import os
 
 def convert_docx_to_html(input_file: str, output_file: str):
-    # Загружаем документ
+    
     doc = Document(input_file)
     
-    # Начинаем формировать HTML
+    level = 0
     html_content = [
         "<!DOCTYPE html>", "<html lang='en'>", "\t<head>", "\t\t<meta charset='UTF-8'>", 
         "\t\t<title>Title</title>", "\t</head>", "\t<body>"
     ]
-    
+
     for paragraph in doc.paragraphs:
-        # Определяем стиль параграфа (например, заголовки)
+        if not paragraph.text.strip():
+            continue
+
         if paragraph.style.name.startswith("Heading"):
-            level = int(paragraph.style.name[-1])  # Уровень заголовка (например, h1, h2)
-            html_content.append(f"\t\t<h{level}>{format_runs(paragraph.runs)}</h{level}>")
+            level = int(paragraph.style.name[-1])
+            html_content.append((level + 1) * "\t" + f'<h{level} class="article-header{level}">{format_runs(paragraph.runs)}</h{level}>')
         else:
-            html_content.append(f"\t\t<p>{format_runs(paragraph.runs)}</p>")
+            if paragraph.runs != "":
+                html_content.append((level + 2) * "\t" + f'<p class="artile-paragraph">{format_runs(paragraph.runs)}</p>')
+            else:
+                html_content.append("\n")
     
     html_content.append("\t</body>")
     html_content.append("</html>")
     
-    # Сохраняем HTML в файл
     with open(output_file, "w", encoding="utf-8") as f:
         f.write("\n".join(html_content))
 
@@ -35,20 +39,20 @@ def format_runs(runs):
     for run in runs:
         text = run.text
         if run.bold:
-            text = f"<b>{text}</b>"
+            text = f'<b class="bold-text">{text}</b>'
         if run.italic:
-            text = f"<i>{text}</i>"
+            text = f'<i class="italic-text">{text}</i>'
         if run.underline:
-            text = f"<u>{text}</u>"
+            text = f'<u class="underline-text">{text}</u>'
         formatted_text += text
     return formatted_text
 
 if __name__ == "__main__":
-    input_path = os.path.abspath("article.docx")
-    output_path = "article.html"
+    input_path = os.path.abspath("parsing/article.docx")
+    output_path = "parsing/article.html"
     
     if Path(input_path).suffix.lower() != ".docx":
-        print("Скрипт поддерживает только .docx файлы.")
+        print("Скрипт поддерживает только .docx файлы")
     else:
         convert_docx_to_html(input_path, output_path)
         print(f"Файл успешно преобразован в HTML и сохранён в {output_path}")
