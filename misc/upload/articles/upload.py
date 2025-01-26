cfe7332d5896f0374c59f5c6d8e2da798c872cec
Julian Zimin <julianzimin@gmail.com>
2025-01-26 18:11:25 +0300

    created script to upload articles to database

--- misc/upload/articles/upload.py
+++ misc/upload/articles/upload.py
@@ -1,67 +1,86 @@
 import os
+import sys
 import shutil
+import asyncio
 from pathlib import Path
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+
+from sqlalchemy import select
+
+from src.database import async_session
 from src.articles.models import Articles
+from src.books.models import Themes
 from misc.utils import convert_docx_to_html
 
 
-PROD_DIR = Path("/static/images/")
-BASE_DIR = Path("/misc/upload/articles")
+PROD_DIR = Path("static/images/")
+BASE_DIR = Path("misc/upload/articles")
 DOCX_DIR = BASE_DIR / "docx_files"
 PREVIEWS_DIR = BASE_DIR / "previews"
 HEADER_IMAGES_DIR = BASE_DIR / "header_images"
 
-docx_files = sorted(DOCX_DIR.glob("*.docx"))
-preview_files = sorted(PREVIEWS_DIR.glob("*"))
-header_files = sorted(HEADER_IMAGES_DIR.glob("*"))
 
-print(docx_files)
-print(preview_files)
-print(header_files)
+async def upload_articles(
+    docx_dir: Path, 
+    previews_dir: Path, 
+    header_images_dir: Path
+):
+    docx_files = sorted(docx_dir.glob("*.docx"))
+    preview_files = sorted(previews_dir.glob("*"))
+    header_images_files = sorted(header_images_dir.glob("*"))
 
-# # Создаем статьи, перемещаем изображения
-# articles = []
-# for docx_file, preview_file, header_file in zip(docx_files, preview_files, header_files):
-#     # Читаем текст из .docx
-#     text = read_docx(docx_file)
-
-#     # Создаем объект модели
-#     article = Articles(text, preview_file.name, header_file.name)
-#     articles.append(article)
-
-#     # Перемещаем изображения
-#     shutil.move(preview_file, PROD_DIR / preview_file.name)
-#     shutil.move(header_file, PROD_DIR / header_file.name)
-
-# # Вывод результата
-# for article in articles:
-#     print(article)
-
-
-
-# def prepare_articles(
-#     theme_id: int,
-#     docx_dir: str = '/misc/docx_files/to_db'
-# ):
-#     docx_files = Path(docx_dir).glob("*.docx")
-#     n = 1
-#     articles = []
-#     for docx in docx_files:
-#         parsed_docx = convert_docx_to_html(docx)
-#         articles.append(
-#             Articles(
-#                 theme = theme_id,
-#                 title = parsed_docx["title"],
-#                 summary = parsed_docx["summary"],
-#                 text = parsed_docx["html_content"],
-#                 created_at = parsed_docx["pub_date"],
-#                 preview = ...,
-#                 header_image = ...,
-#                 read_time = parsed_docx["read_time"],
-#                 active = True
-#             )
-#         )
-#         n += 1
+    articles = []
+    for docx, preview, header_image in zip(
+        docx_files, 
+        preview_files, 
+        header_images_files
+    ):
+        parsed_docx = convert_docx_to_html(docx)
+        theme_name = parsed_docx["theme"]
         
-#     return articles
+        query = (
+            select(Themes.id)
+            .where(Themes.name == theme_name)
+        )
+        async with async_session() as session:
+            result = await session.execute(query)
+            theme_id = result.scalar()
+            if not theme_id:
+                new_theme = Themes(name=theme_name)
+                session.add(new_theme)
+                await session.commit()
+                await session.refresh(new_theme)
+                theme_id = new_theme.id
+
+        articles.append(
+            Articles(
+                theme = theme_id,
+                title = parsed_docx["title"],
+                summary = parsed_docx["summary"],
+                text = parsed_docx["html_content"],
+                created_at = parsed_docx["pub_date"],
+                preview = preview.name,
+                header_image = header_image.name,
+                read_time = parsed_docx["read_time"],
+                active = True
+            )
+        )
+
+        async with async_session() as session:
+            session.add_all(articles)
+            await session.commit()
+            
+        shutil.move(preview, PROD_DIR / 'previews' / preview.name)
+        shutil.move(header_image, PROD_DIR / 'header_images' / header_image.name)
+        # docx.unlink()
+
+
+if __name__ == "__main__":
+    asyncio.run(
+        upload_articles(
+            docx_dir=DOCX_DIR,
+            previews_dir=PREVIEWS_DIR,
+            header_images_dir=HEADER_IMAGES_DIR
+        )
+    )
