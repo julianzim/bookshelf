3ac99d8fb61ff8a469580494cd0582acd99e6602
Julian Zimin <julianzimin@gmail.com>
2025-02-08 16:03:15 +0300

    added getting secrets for fastapi mail

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
+from pydantic import BaseModel, EmailStr
+from fastapi_mail import ConnectionConfig
 
 load_dotenv()
 
@@ -12,3 +14,26 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 
 SECRET_AUTH = os.environ.get("SECRET_AUTH")
+
+
+class EmailConfig(BaseModel):
+    MAIL_USERNAME: str = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD: str = os.environ.get("MAIL_PASSWORD")
+    MAIL_FROM: EmailStr = os.environ.get("MAIL_FROM")
+    MAIL_PORT: int = int(os.environ.get("MAIL_PORT"))  # 587 for TLS, 465 for SSL
+    MAIL_SERVER: str = os.environ.get("MAIL_SERVER")
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+
+email_config = EmailConfig()
+
+mail_conf = ConnectionConfig(
+    MAIL_USERNAME=email_config.MAIL_USERNAME,
+    MAIL_PASSWORD=email_config.MAIL_PASSWORD,
+    MAIL_FROM=email_config.MAIL_FROM,
+    MAIL_PORT=email_config.MAIL_PORT,
+    MAIL_SERVER=email_config.MAIL_SERVER,
+    MAIL_STARTTLS=email_config.MAIL_STARTTLS,
+    MAIL_SSL_TLS=email_config.MAIL_SSL_TLS,
+    USE_CREDENTIALS=True
+)
