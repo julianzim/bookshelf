6c3ce95499c352a331357bd2092867401a560e6b
Julian Zimin <julianzimin@gmail.com>
2025-05-02 13:52:57 +0300

    feat (tests): Pytest coverage
    Created fixture for AsyncClient context manager

--- tests/test_books.py
+++ tests/test_books.py
@@ -2,41 +2,42 @@ import pytest
 
 from httpx import AsyncClient, ASGITransport
 from bs4 import BeautifulSoup
+from typing import AsyncGenerator
 
 from src.main import app
 
 
-@pytest.mark.asyncio
-async def test_get_books():
+@pytest.fixture
+async def async_client() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(
         transport=ASGITransport(app=app),
         base_url="http://test"
     ) as ac:
-        response = await ac.get("/books")
-        assert response.status_code == 200
+        yield ac
 
-        soup = BeautifulSoup(response.text, 'html.parser')
 
-        books = soup.find_all('div', class_='book-item')
-        assert len(books) == 15
+@pytest.mark.asyncio
+async def test_get_books(async_client: AsyncClient) -> None:
+    response = await async_client.get("/books")
+    assert response.status_code == 200
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    books = soup.find_all('div', class_='book-item')
+    assert len(books) == 15
         
 
-
 @pytest.mark.asyncio
-async def test_get_book_details():
-    async with AsyncClient(
-        transport=ASGITransport(app=app),
-        base_url="http://test"
-    ) as ac:
-        response = await ac.get(f"/books/My%20Friend%20Joy")
-        assert response.status_code == 200
+async def test_get_book_details(async_client: AsyncClient) -> None:
+    response = await async_client.get(f"/books/My%20Friend%20Joy")
+    assert response.status_code == 200
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-        title = soup.find('h1', class_='book-title-right')
-        assert 'My Friend Joy' in title.text
+    title = soup.find('h1', class_='book-title-right')
+    assert 'My Friend Joy' in title.text
 
-        img = soup.find('img', class_='book-cover-details')
-        assert '/static/images/covers/MyFriendJoy.png' in img['src']
+    img = soup.find('img', class_='book-cover-details')
+    assert '/static/images/covers/MyFriendJoy.png' in img['src']
 
         
