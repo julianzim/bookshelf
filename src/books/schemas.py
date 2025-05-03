f05ef6c01f21c10f630cf7bcfad3fb52c54f2022
Julian Zimin <julianzimin@gmail.com>
2025-05-03 14:35:18 +0300

    feat(valid): Pydantic validation
    Created books schemas

--- src/books/schemas.py
+++ src/books/schemas.py
@@ -1,7 +1,28 @@
-from pydantic import BaseModel
+import datetime
+
+from pydantic import BaseModel, ConfigDict, Field, HttpUrl
 
 
-class GetAllBooks(BaseModel):
-    id: int
+class BaseBook(BaseModel):
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+class BookCard(BaseBook):
     title: str
-    image: str
+    cover: str = Field(pattern=r".+\.(png|jpg|jpeg)$")
+    active: bool
+
+
+class BookDetail(BookCard):
+    series: str
+    pub_date: datetime.datetime
+    amazon_link: HttpUrl
+    aloud_link: HttpUrl
+    eb_price: float = Field(ge=0.0)
+    pb_price: float = Field(ge=0.0)
+    summary: str = Field(max_length=2000)
+    language: str = Field(max_length=23)
+    min_age: int = Field(gt=0, lt=100)
+    max_age: int = Field(gt=0, lt=100)
+    pages: int = Field(gt=0)
