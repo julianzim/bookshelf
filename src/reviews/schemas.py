4ccb5df32aa725026bc307e1abbb5a3271c7e664
Julian Zimin <julianzimin@gmail.com>
2025-05-03 17:08:12 +0300

    feat(valid): Reviews Pydantic validation
    Created reviews schemas

--- src/reviews/schemas.py
+++ src/reviews/schemas.py
@@ -1,7 +1,22 @@
-from pydantic import BaseModel, Field
+import datetime
+
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class ReviewCreate(BaseModel):
     title: str = Field(min_length=3, max_length=100, description="Title of the review")
     text: str = Field(min_length=10, max_length=1000, description="Text of the review")
     rating: int = Field(ge=1, le=5, description="Rating between 1 and 5")
+
+
+class ReviewOut(ReviewCreate):
+    username: str = Field(min_length=1, max_length=100)
+    created_at: datetime.datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BookReviewStats(BaseModel):
+    average_rating: float
+    ratings_count: int
+    reviews_count: int
