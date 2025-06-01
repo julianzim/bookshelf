29fcedd44492f6a71fedeadf03ba3950808642f6
Julian Zimin <julianzimin@gmail.com>
2025-06-01 15:30:49 +0300

    feat(queries.py): Review Moderation
    Implemented joinedload in get_review_by_id query

--- src/queries.py
+++ src/queries.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException
 from sqlalchemy import select, join, desc, asc
+from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.models import User
@@ -7,7 +8,7 @@ from src.auth.schemas import UserRead
 from src.books.models import Books, Themes
 from src.books.schemas import BookCard, BookDetail
 from src.reviews.models import Reviews
-from src.reviews.schemas import ReviewOut
+from src.reviews.schemas import ReviewOut, ReviewOutRel
 from src.articles.models import Articles
 from src.articles.schemas import ArticleCard, ArticleDetail, ThemeDetail
 from src.database import async_engine, Base
@@ -28,7 +29,7 @@ async def reset_database():
 async def get_all_books(session: AsyncSession) -> list[BookCard]:
     query = select(Books).order_by(asc(Books.pub_date))
     result = await session.execute(query)
-    
+
     books_orm = result.scalars().all()
     books = [BookCard.model_validate(book) for book in books_orm]
 
@@ -41,7 +42,7 @@ async def get_book_by_title(title: str, session: AsyncSession) -> BookDetail | N
 
     book_orm = result.scalars().first()
     if book_orm is None:
-        logger.error(f"Book \"{title}\" not found")
+        logger.error("Book \"%s\" not found", title)
         raise HTTPException(status_code=404, detail=f"Book \"{title}\" not found")
 
     book = BookDetail.model_validate(book_orm)
@@ -54,12 +55,12 @@ async def get_related_books_by_title(title: str, session: AsyncSession) -> list[
         select(Books)
         .where(
             Books.title != title,
-            Books.active == True
+            Books.active is True
         )
         .order_by(asc(Books.pub_date))
     )
     result = await session.execute(query)
-    
+
     books_orm = result.scalars().all()
     books = [BookCard.model_validate(book) for book in books_orm]
 
@@ -74,11 +75,15 @@ async def get_all_book_reviews(
 ) -> list[ReviewOut]:
     valid_order_columns = ['created_at', 'rating']
     valid_order_methods = ['asc', 'desc']
-    
+
     if order_by not in valid_order_columns:
-        raise ValueError(f"Invalid order_by value. Must be one of {valid_order_columns}. Got {order_by}")
+        raise ValueError(
+            f"Invalid order_by value. Must be one of {valid_order_columns}. Got {order_by}"
+        )
     if order_method not in valid_order_methods:
-        raise ValueError(f"Invalid order_method value. Must be one of {valid_order_methods}. Got {order_method}")
+        raise ValueError(
+            f"Invalid order_method value. Must be one of {valid_order_methods}. Got {order_method}"
+        )
 
     order_func = asc if order_method == 'asc' else desc
 
@@ -94,39 +99,54 @@ async def get_all_book_reviews(
             join(Reviews, Books, Reviews.book_id == Books.id)
             .join(User, Reviews.user_id == User.id)
         )
-        .where(Books.title == title, Reviews.approved == True)
+        .where(Books.title == title, Reviews.approved is True)
         .order_by(order_func(getattr(Reviews, order_by)))
     )
     result = await session.execute(query)
-    
+
     reviews_orm = result.fetchall()
     reviews = [ReviewOut.model_validate(review) for review in reviews_orm]
-    
+
     return reviews
 
 
 async def get_review_by_id(
-    id: int,
+    review_id: int,
     session: AsyncSession
 ) -> Reviews:
-    query = select(Reviews).where(Reviews.id == id)
+    query = (
+        select(Reviews)
+        .options(
+            joinedload(Reviews.user),
+            joinedload(Reviews.book)
+        )
+        .where(Reviews.id == review_id)
+    )
     result = await session.execute(query)
-    
-    review = result.scalars().first()
-    if not review:
-        raise HTTPException(status_code=404, detail=f"Review {id} not found")
-    
-    if review.published:
-        logger.error(f"Re-moderation is not allowed — review {id} has already been moderated and published.")
-        raise HTTPException(status_code = 400, detail = f"Re-moderation is not allowed — review {id} has already been moderated and published.")
-    
-    return review
+    review_orm = result.unique().scalars().first()
+    if not review_orm:
+        raise HTTPException(status_code=404, detail=f"Review {review_id} not found")
+
+    if review_orm.published:
+        logger.error(
+            "Re-moderation is not allowed — review %s has already been moderated and published.",
+            review_id
+        )
+        raise HTTPException(
+            status_code = 400,
+            detail = (
+                f"Re-moderation is not allowed — review {review_id} has already been moderated "
+                f"and published."
+            )
+        )
+    # review = ReviewOutRel.model_validate(review_orm)
+    return review_orm
 
 
 async def get_active_articles(session: AsyncSession) -> list[ArticleCard]:
     query = (
         select(Articles)
-        .where(Articles.active == True)
+        .where(Articles.active is True)
         .order_by(desc(Articles.created_at))
     )
     result = await session.execute(query)
@@ -146,29 +166,29 @@ async def get_article_by_id(
         .join(Themes, Articles.theme == Themes.id)
         .where(
             Articles.id == id,
-            Articles.active == True
+            Articles.active is True
         )
     )
     result = await session.execute(query)
     if result is None:
-        logger.error(f"Article id={id} not found")
+        logger.error("Article id=%s not found", id)
         raise HTTPException(status_code=404, detail=f"Article id={id} not found")
 
     article_orm, theme_orm = result.first()
     article = ArticleDetail.model_validate(article_orm)
     theme = ThemeDetail.model_validate(theme_orm)
-    
+
     return article, theme
 
 
-async def get_user_by_id(id: int, session: AsyncSession) -> UserRead:
-    query = select(User).where(User.id == id)
+async def get_user_by_id(user_id: int, session: AsyncSession) -> UserRead:
+    query = select(User).where(User.id == user_id)
     result = await session.execute(query)
-    
+
     user_orm = result.scalars().first()
     if user_orm is None:
-        logger.error(f"User id={id} not found")
-        raise HTTPException(status_code=404, detail=f"User id={id} not found")
+        logger.error("User id=%s not found", user_id)
+        raise HTTPException(status_code=404, detail=f"User id={user_id} not found")
 
     user = UserRead.model_validate(user_orm)
 
