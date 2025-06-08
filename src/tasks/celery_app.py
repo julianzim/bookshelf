a8d6bf900b3a131fca277f1027ad10220c78bae7
Julian Zimin <julianzimin@gmail.com>
2025-06-08 16:37:09 +0300

    feat(celery): Background tasks
    Used option task_always_eager=True when CELERY_DEV_MODE is True

--- src/tasks/celery_app.py
+++ src/tasks/celery_app.py
@@ -9,3 +9,8 @@ app = Celery(
     include=["src.tasks.email_tasks"],
     broker_connection_retry_on_startup=True
 )
+
+app.conf.update(
+    task_always_eager=app_config.CELERY_DEV_MODE,
+    task_eager_propagates=app_config.CELERY_DEV_MODE
+)
